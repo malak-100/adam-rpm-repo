fe144848fe9dabbfadf051e68048cd3ce744d24f
Malak_99 <160192136+malak-100@users.noreply.github.com>
2024-06-14 18:27:49 +0100

    Update bash_script3.sh

--- bash_script3.sh
+++ bash_script3.sh
@@ -4,3 +4,5 @@
 echo " " > data/exams_datahovuhovi.txt  
 echo " " > data/students_datahovuhovi.txt  
 echo " " > data/teachers_datahovuhovi.txt
+echo " " > data/exams_next_datahovuhovi.txt
+echo " " > data/exams_next_next_datahovuhovi.txt
