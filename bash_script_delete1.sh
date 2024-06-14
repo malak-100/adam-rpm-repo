22717c2964000614a58cd21c260872533b390bd1
Malak_99 <160192136+malak-100@users.noreply.github.com>
2024-06-14 18:30:18 +0100

    Update bash_script_delete1.sh

--- bash_script_delete1.sh
+++ bash_script_delete1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## exams deletion  
 
-echo " " > data/exams_datahovuhovi.txt 
-echo " " > data/exams_next_datahovuhovi.txt
-echo " " > data/exams_next_next_datahovuhovi.txt
+echo " " > /usr/share/adam/data/exams_datahovuhovi.txt 
+echo " " > /usr/share/adam/data/exams_next_datahovuhovi.txt
+echo " " > /usr/share/adam/data/exams_next_next_datahovuhovi.txt
